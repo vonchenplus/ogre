9abe90058e9fdaab5548d3fc2a48d18785eccba2
dark_sylinc <dark_sylinc@localhost>
2010-08-22 19:42:34 -0300

    Added support for 32-bit index buffers to InstancedBatchShader Added more documentation to classes InstanceManager now successfully deals internally with HW skinned meshes Added _underscore to updateBatches() function, to denote it's for internal usage

---
+++ OgreMain/include/OgreInstanceManager.h
@@ -0,0 +1,142 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2009 Torus Knot Software Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+#ifndef __InstanceManager_H__
+#define __InstanceManager_H__
+
+#include "OgrePrerequisites.h"
+
+namespace Ogre
+{
+	/** \addtogroup Core
+	*  @{
+	*/
+	/** \addtogroup Scene
+	*  @{
+	*/
+
+	/** This is the main starting point for the new instancing system.
+		Each InstanceManager can control one technique and one mesh, but it can manage
+		multiple materials at the same time.
+		@See SceneManager::createInstanceManager, which creates this InstanceManager. Each
+		must have a unique name. It's wasteless to create two InstanceManagers with the same
+		mesh reference, instancing technique and instances per batch count.
+		This class takes care of managing batches automatically, so that more are created when
+		needed, and reuse existing ones as much as posible; thus the user doesn't have to worry
+		of managing all those low level issues.
+		@See InstanceBatch & @see InstanceEntity for more information.
+
+        @remarks
+			Design discussion webpage: http://www.ogre3d.org/forums/viewtopic.php?f=4&t=59902
+        @author
+            Matias N. Goldberg ("dark_sylinc")
+        @version
+            1.0
+     */
+	class _OgreExport InstanceManager : public FactoryAlloc
+	{
+	public:
+		enum InstancingTechnique
+		{
+			ShaderBased,			//Any SM 2.0+
+			HardwareInstancing,		//Needs SM 3.0+ and HW instancing support
+		};
+	private:
+		typedef vector<InstanceBatch*>::type		InstanceBatchVec;	//vec[batchN] = Batch
+		typedef map<String, InstanceBatchVec>::type	InstanceBatchMap;	//map[materialName] = Vec
+
+		const String			m_name;					//Not the name of the mesh
+		MeshPtr					m_meshReference;
+		InstanceBatchMap		m_instanceBatches;
+		size_t					m_idCount;
+
+		RenderOperation			m_sharedRenderOperation;
+
+		size_t					m_instancesPerBatch;
+		InstancingTechnique		m_instancingTechnique;
+
+		SceneManager			*m_sceneManager;
+
+		/** Finds a batch with at least one free instanced entity we can use.
+			If none found, creates one.
+		*/
+		inline InstanceBatch* getFreeBatch( const String &materialName );
+
+		/** Called when creating the first batch for the first time. This can take big build time.
+			It takes care of getting the render operation which will be shared by further batches,
+			which decreases their build time, and prevents GPU RAM from skyrocketing.
+			@See InstanceBatch on how buffers are shared.
+			@param materialName The material name, to know where to put this batch in the map
+			@returns The created InstancedManager for convenience
+        */
+		InstanceBatch* buildFirstTime( const String &materialName );
+
+		/** Called when batches are fully exhausted (can't return more instances) so a new batch
+			is created. For the first time use, @see buildFirstTime
+			@param materialName The material name, to know where to put this batch in the map
+			@returns The created InstancedManager for convenience
+        */
+		InstanceBatch* buildNewBatch( const String &materialName );
+
+	public:
+		InstanceManager( const String &customName, SceneManager *sceneManager,
+						 const String &meshName, const String &groupName,
+						 InstancingTechnique instancingTechnique, size_t instancesPerBatch );
+		virtual ~InstanceManager();
+
+		const String& getName() const { return m_name; }
+
+		/** Returns whether the specified RenderTarget is compatible with this DepthBuffer
+			That is, this DepthBuffer can be attached to that RenderTarget
+            @remarks
+                Most APIs impose the following restrictions:
+				Width & height must be equal or higher than the render target's
+				They must be of the same bit depth.
+				They need to have the same FSAA setting
+			@param renderTarget The render target to test against
+        */
+		InstancedEntity* createInstancedEntity( const String &materialName );
+
+		/** This function can be usefull to improve CPU speed after having too many instances
+			created, which where now removed, thus freeing many batches with zero used Instanced Entities
+			However the batches aren't automatically removed from memory until the InstanceManager is
+			destroyed, or this function is called. This function removes those batches which are completely
+			unused (only wasting memory).
+        */
+		void cleanupEmptyBatches(void);
+
+		/** Should be called once per frame to update all batches' data like bounds. Unfortunately
+			we can't leave this automatically by Ogre because Ogre updates on request when
+			a SceneNode change position, scale, etc; but these bounds also depend on camera's
+			position and direction, which makes updating the whole batch almost every frame anyway.
+			The bbox is updated dynamically for better shadow accuracy.
+		*/
+		void _updateBatches(void);
+	};
+}
+
+#endif
---
+++ OgreMain/src/OgreInstanceManager.cpp
@@ -0,0 +1,225 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2009 Torus Knot Software Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+#include "OgreStableHeaders.h"
+#include "OgreInstanceManager.h"
+#include "OgreInstanceBatchShader.h"
+#include "OgreMesh.h"
+#include "OgreSubMesh.h"
+#include "OgreMeshManager.h"
+#include "OgreMaterialManager.h"
+#include "OgreSceneManager.h"
+
+namespace Ogre
+{
+	InstanceManager::InstanceManager( const String &customName, SceneManager *sceneManager,
+										const String &meshName, const String &groupName,
+										InstancingTechnique instancingTechnique, size_t instancesPerBatch ) :
+				m_name( customName ),
+				m_sceneManager( sceneManager ),
+				m_idCount( 0 ),
+				m_instancesPerBatch( instancesPerBatch ),
+				m_instancingTechnique( instancingTechnique )
+	{
+		m_meshReference = MeshManager::getSingleton().load( meshName, groupName );
+
+		if( m_meshReference->hasSkeleton() && !m_meshReference->getSkeleton().isNull() )
+			m_meshReference->getSubMesh(0)->_compileBoneAssignments();
+	}
+
+	InstanceManager::~InstanceManager()
+	{
+		//Remove all batches from all materials we created
+		InstanceBatchMap::const_iterator itor = m_instanceBatches.begin();
+		InstanceBatchMap::const_iterator end  = m_instanceBatches.end();
+
+		while( itor != end )
+		{
+			InstanceBatchVec::const_iterator it = itor->second.begin();
+			InstanceBatchVec::const_iterator en = itor->second.end();
+
+			while( it != en )
+				OGRE_DELETE *it++;
+
+			++itor;
+		}
+
+		//Free the shared RenderOperation since it's our responsability
+		if( m_sharedRenderOperation.vertexData )
+			OGRE_DELETE m_sharedRenderOperation.vertexData;
+		if( m_sharedRenderOperation.indexData )
+			OGRE_DELETE m_sharedRenderOperation.indexData;
+	}
+	//----------------------------------------------------------------------
+	InstancedEntity* InstanceManager::createInstancedEntity( const String &materialName )
+	{
+		InstanceBatch *instanceBatch;
+
+		if( m_instanceBatches.empty() )
+			instanceBatch = buildFirstTime( materialName );
+		else
+			instanceBatch = getFreeBatch( materialName );
+
+		return instanceBatch->createInstancedEntity();
+	}
+	//-----------------------------------------------------------------------
+	inline InstanceBatch* InstanceManager::getFreeBatch( const String &materialName )
+	{
+		InstanceBatchVec &batchVec = m_instanceBatches[materialName];
+
+		InstanceBatchVec::const_reverse_iterator itor = batchVec.rbegin();
+		InstanceBatchVec::const_reverse_iterator end  = batchVec.rend();
+
+		while( itor != end )
+		{
+			if( !(*itor)->isBatchFull() )
+				return *itor;
+			++itor;
+		}
+
+		//None found, or they're all full
+		return buildNewBatch( materialName );
+	}
+	//-----------------------------------------------------------------------
+	InstanceBatch* InstanceManager::buildFirstTime( const String &materialName )
+	{
+		Mesh::IndexMap &idxMap = m_meshReference->getSubMesh(0)->blendIndexToBoneIndexMap;
+		idxMap = idxMap.empty() ? m_meshReference->sharedBlendIndexToBoneIndexMap : idxMap;
+
+		MaterialPtr mat = MaterialManager::getSingleton().getByName( materialName,
+																	m_meshReference->getGroup() );
+
+		InstanceBatchVec &materialInstanceBatch = m_instanceBatches[materialName];
+
+		InstanceBatchShader *batch = OGRE_NEW InstanceBatchShader( m_meshReference, mat, m_instancesPerBatch,
+																	&idxMap, m_name + "/InstanceBatch_" +
+																	StringConverter::toString(m_idCount++) );
+
+		m_instancesPerBatch = std::min( m_instancesPerBatch,
+										batch->calculateMaxNumInstances( m_meshReference->getSubMesh(0) ) );
+		batch->_setInstancesPerBatch( m_instancesPerBatch );
+
+		//TODO: Create a "merge" function that merges all submeshes into one big submesh
+		//instead of just sending submesh #0
+		//Get the RenderOperation to be shared with further instances.
+		m_sharedRenderOperation = batch->build( m_meshReference->getSubMesh(0) );
+
+		//Batches need to be part of a scene node so that their renderable can be rendered
+		SceneNode *sceneNode = m_sceneManager->getRootSceneNode()->createChildSceneNode();
+		sceneNode->attachObject( batch );
+		//sceneNode->showBoundingBox( true );
+
+		materialInstanceBatch.push_back( batch );
+
+		return batch;
+	}
+	//-----------------------------------------------------------------------
+	InstanceBatch* InstanceManager::buildNewBatch( const String &materialName )
+	{
+		Mesh::IndexMap &idxMap = m_meshReference->getSubMesh(0)->blendIndexToBoneIndexMap;
+		idxMap = idxMap.empty() ? m_meshReference->sharedBlendIndexToBoneIndexMap : idxMap;
+
+		MaterialPtr mat = MaterialManager::getSingleton().getByName( materialName,
+																	m_meshReference->getGroup() );
+
+		InstanceBatchVec &materialInstanceBatch = m_instanceBatches[materialName];
+
+		InstanceBatchShader *batch = OGRE_NEW InstanceBatchShader( m_meshReference, mat, m_instancesPerBatch,
+																	&idxMap, m_name + "/InstanceBatch_" +
+																	StringConverter::toString(m_idCount++) );
+
+		//TODO: Check different materials have the same m_instancesPerBatch upper limit
+		//otherwise we can't share
+		batch->buildFrom( m_meshReference->getSubMesh(0), m_sharedRenderOperation );
+
+		//Batches need to be part of a scene node so that their renderable can be rendered
+		SceneNode *sceneNode = m_sceneManager->getRootSceneNode()->createChildSceneNode();
+		sceneNode->attachObject( batch );
+		//sceneNode->showBoundingBox( true );
+
+		materialInstanceBatch.push_back( batch );
+
+		return batch;
+	}
+	//-----------------------------------------------------------------------
+	void InstanceManager::cleanupEmptyBatches(void)
+	{
+		InstanceBatchMap::iterator itor = m_instanceBatches.begin();
+		InstanceBatchMap::iterator end  = m_instanceBatches.end();
+
+		while( itor != end )
+		{
+			InstanceBatchVec::iterator it = itor->second.begin();
+			InstanceBatchVec::iterator en = itor->second.end();
+
+			while( it != en )
+			{
+				if( !(*it)->isBatchUnused() )
+				{
+					OGRE_DELETE *it;
+					//Remove it from the list swapping with the last element and popping back
+					size_t idx = it - itor->second.begin();
+					*it = itor->second.back();
+					itor->second.pop_back();
+
+					//Restore invalidated iterators
+					it = itor->second.begin() + idx;
+					en = itor->second.end();
+				}
+				else
+					++it;
+			}
+
+			++itor;
+		}
+
+		//By this point it may happen that all m_instanceBatches' objects are also empty
+		//however if we call m_instanceBatches.clear(), next time we'll create an InstancedObject
+		//we'll end up calling buildFirstTime() instead of buildNewBatch(), which is not the idea
+		//(takes more time and will leak the shared render operation)
+	}
+	//-----------------------------------------------------------------------
+	void InstanceManager::_updateBatches(void)
+	{
+		InstanceBatchMap::iterator itor = m_instanceBatches.begin();
+		InstanceBatchMap::iterator end  = m_instanceBatches.end();
+
+		while( itor != end )
+		{
+			InstanceBatchVec::iterator it = itor->second.begin();
+			InstanceBatchVec::iterator en = itor->second.end();
+
+			while( it != en )
+			{
+				(*it)->updateBounds();
+				++it;
+			}
+
+			++itor;
+		}
+	}
+}
