08bda4eaecbbaeca4b3e92e0fe1e67985e72789a
Mattan Furst <matanf@simigon.com>
2011-08-21 19:04:16 +0300

    Fixed Lights disapearing when using default min pixel size

---
+++ Samples/NewInstancing/include/NewInstancing.h
@@ -0,0 +1,120 @@
+#ifndef __NewInstancing_H__
+#define __NewInstancing_H__
+
+#include "SdkSample.h"
+#include "OgreInstancedEntity.h"
+
+using namespace Ogre;
+using namespace OgreBites;
+
+static const char *c_instancingTechniques[] =
+{
+	"Shader Based",
+	"Vertex Texture Fetch (VTF)",
+	"Hardware Instancing Basic",
+	"Hardware Instancing + VTF",
+	"Limited Animation - Hardware Instancing + VTF",
+	"No Instancing"
+};
+
+static const char *c_materialsTechniques[] =
+{
+	"Examples/Instancing/ShaderBased/Robot",
+	"Examples/Instancing/VTF/Robot",
+	"Examples/Instancing/HWBasic/Robot",
+	"Examples/Instancing/VTF/HW/Robot",
+	"Examples/Instancing/VTF/HW/LUT/Robot",
+	"Examples/Instancing/ShaderBased/Robot"
+};
+
+static const char *c_meshNames[] =
+{
+	"robot.mesh"
+};
+
+#define NUM_TECHNIQUES (((int)InstanceManager::InstancingTechniquesCount) + 1)
+
+class _OgreSampleClassExport Sample_NewInstancing : public SdkSample
+{
+public:
+
+	Sample_NewInstancing();
+
+    bool frameRenderingQueued(const FrameEvent& evt);
+
+	bool keyPressed(const OIS::KeyEvent& evt);
+	
+
+protected:
+	void setupContent();
+
+	void setupLighting();
+	
+	void switchInstancingTechnique();
+
+	void createEntities();
+
+	void createInstancedEntities();
+
+	void createSceneNodes();
+	
+	void clearScene();
+
+	void destroyManagers();
+
+	void cleanupContent();
+
+	void animateUnits( float timeSinceLast );
+
+	void moveUnits( float timeSinceLast );
+
+	//Helper function to look towards normDir, where this vector is normalized, with fixed Yaw
+	Quaternion lookAt( const Vector3 &normDir );
+
+	void defragmentBatches();
+
+	void setupGUI();
+
+	void itemSelected(SelectMenu* menu);
+
+	void buttonHit( OgreBites::Button* button );
+
+	void checkBoxToggled(CheckBox* box);
+
+	void sliderMoved(Slider* slider);
+
+	void testCapabilities(const RenderSystemCapabilities* caps);
+
+	//The difference between testCapabilities() is that features checked here aren't fatal errors.
+	//which means the sample can run (with limited functionality) on those computers
+	void checkHardwareSupport();
+
+	//You can also use a union type to switch between Entity and InstancedEntity almost flawlessly:
+	/*
+	union FusionEntity
+	{
+		Entity			entity
+		InstancedEntity	instancedEntity;
+	};
+	*/
+	int NUM_INST_ROW;
+	int NUM_INST_COLUMN;
+	int								mInstancingTechnique;
+	int								mCurrentMesh;
+	std::vector<MovableObject*>		mEntities;
+	std::vector<SceneNode*>			mSceneNodes;
+	std::set<AnimationState*>		mAnimations;
+	InstanceManager					*mCurrentManager;
+	bool							mSupportedTechniques[NUM_TECHNIQUES+1];
+
+	SelectMenu						*mTechniqueMenu;
+	CheckBox						*mMoveInstances;
+	CheckBox						*mAnimateInstances;
+	CheckBox						*mEnableShadows;
+	CheckBox						*mSetStatic;
+	OgreBites::Button				*mDefragmentBatches;
+	CheckBox						*mDefragmentOptimumCull;
+	Slider							*mInstancesSlider;
+};
+
+#endif
