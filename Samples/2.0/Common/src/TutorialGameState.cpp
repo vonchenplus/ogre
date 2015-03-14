cb2a6c1d1bcd51f55884176f7070e3af548f2631
Tom?? Kov?? <brendorrmt@gmail.com>
2015-03-14 12:47:08 +0100

    Shader compilation errors now dump the source being compiled to the log, making the debuggning of HLSL shaders much easier. Also corrected unsigned literals that would not compile on nvidia cards.

---
+++ Samples/2.0/Common/src/TutorialGameState.cpp
@@ -0,0 +1,152 @@
+
+#include "TutorialGameState.h"
+#include "CameraController.h"
+#include "GraphicsSystem.h"
+
+#include "OgreSceneManager.h"
+
+#include "OgreOverlayManager.h"
+#include "OgreOverlay.h"
+#include "OgreOverlayContainer.h"
+#include "OgreTextAreaOverlayElement.h"
+
+#include "OgreRoot.h"
+#include "OgreFrameStats.h"
+
+using namespace Demo;
+
+namespace Demo
+{
+    TutorialGameState::TutorialGameState( const Ogre::String &helpDescription ) :
+        mGraphicsSystem( 0 ),
+        mCameraController( 0 ),
+        mHelpDescription( helpDescription ),
+        mDisplayHelpMode( 1 ),
+        mNumDisplayHelpModes( 2 ),
+        mDebugText( 0 )
+    {
+    }
+    //-----------------------------------------------------------------------------------
+    void TutorialGameState::_notifyGraphicsSystem( GraphicsSystem *graphicsSystem )
+    {
+        mGraphicsSystem = graphicsSystem;
+    }
+    //-----------------------------------------------------------------------------------
+    void TutorialGameState::createScene01(void)
+    {
+        createDebugTextOverlay();
+    }
+    //-----------------------------------------------------------------------------------
+    void TutorialGameState::createDebugTextOverlay(void)
+    {
+        Ogre::v1::OverlayManager &overlayManager = Ogre::v1::OverlayManager::getSingleton();
+        Ogre::v1::Overlay *overlay = overlayManager.create( "DebugText" );
+
+        Ogre::v1::OverlayContainer *panel = static_cast<Ogre::v1::OverlayContainer*>(
+            overlayManager.createOverlayElement("Panel", "DebugPanel"));
+        mDebugText = static_cast<Ogre::v1::TextAreaOverlayElement*>(
+                    overlayManager.createOverlayElement( "TextArea", "DebugText" ) );
+        mDebugText->setFontName( "DebugFont" );
+        mDebugText->setCharHeight( 0.025f );
+
+        mDebugTextShadow= static_cast<Ogre::v1::TextAreaOverlayElement*>(
+                    overlayManager.createOverlayElement( "TextArea", "0DebugTextShadow" ) );
+        mDebugTextShadow->setFontName( "DebugFont" );
+        mDebugTextShadow->setCharHeight( 0.025f );
+        mDebugTextShadow->setColour( Ogre::ColourValue::Black );
+        mDebugTextShadow->setPosition( 0.002f, 0.002f );
+
+        panel->addChild( mDebugTextShadow );
+        panel->addChild( mDebugText );
+        overlay->add2D( panel );
+        overlay->show();
+    }
+    //-----------------------------------------------------------------------------------
+    void TutorialGameState::generateDebugText( float timeSinceLast, Ogre::String &outText )
+    {
+        if( mDisplayHelpMode == 0 )
+        {
+            outText = mHelpDescription;
+            outText += "\n\nPress F1 to toggle help";
+            return;
+        }
+
+        const Ogre::FrameStats *frameStats = mGraphicsSystem->getRoot()->getFrameStats();
+
+        Ogre::String finalText;
+        finalText.reserve( 128 );
+        finalText  = "Frame time:\t";
+        finalText += Ogre::StringConverter::toString( timeSinceLast * 1000.0f );
+        finalText += " ms\n";
+        finalText += "Frame FPS:\t";
+        finalText += Ogre::StringConverter::toString( 1.0f / timeSinceLast );
+        finalText += "\nAvg time:\t";
+        finalText += Ogre::StringConverter::toString( frameStats->getAvgTime() );
+        finalText += " ms\n";
+        finalText += "Avg FPS:\t";
+        finalText += Ogre::StringConverter::toString( 1000.0f / frameStats->getAvgTime() );
+        finalText += "\n\nPress F1 to toggle help";
+
+        outText.swap( finalText );
+
+        mDebugText->setCaption( finalText );
+        mDebugTextShadow->setCaption( finalText );
+    }
+    //-----------------------------------------------------------------------------------
+    void TutorialGameState::update( float timeSinceLast )
+    {
+        if( mDisplayHelpMode != 0 )
+        {
+            //Show FPS
+            Ogre::String finalText;
+            generateDebugText( timeSinceLast, finalText );
+            mDebugText->setCaption( finalText );
+            mDebugTextShadow->setCaption( finalText );
+        }
+
+        if( mCameraController )
+            mCameraController->update( timeSinceLast );
+    }
+    //-----------------------------------------------------------------------------------
+    void TutorialGameState::keyPressed( const SDL_KeyboardEvent &arg )
+    {
+        bool handledEvent = false;
+
+        if( mCameraController )
+            handledEvent = mCameraController->keyPressed( arg );
+
+        if( !handledEvent )
+            GameState::keyPressed( arg );
+    }
+    //-----------------------------------------------------------------------------------
+    void TutorialGameState::keyReleased( const SDL_KeyboardEvent &arg )
+    {
+        if( arg.keysym.sym == SDLK_F1 )
+        {
+            mDisplayHelpMode = (mDisplayHelpMode + 1) % mNumDisplayHelpModes;
+
+            Ogre::String finalText;
+            generateDebugText( 0, finalText );
+            mDebugText->setCaption( finalText );
+            mDebugTextShadow->setCaption( finalText );
+        }
+        else
+        {
+            bool handledEvent = false;
+
+            if( mCameraController )
+                handledEvent = mCameraController->keyReleased( arg );
+
+            if( !handledEvent )
+                GameState::keyReleased( arg );
+        }
+    }
+    //-----------------------------------------------------------------------------------
+    void TutorialGameState::mouseMoved( const SDL_Event &arg )
+    {
+        if( mCameraController )
+            mCameraController->mouseMoved( arg );
+
+        GameState::mouseMoved( arg );
+    }
+}
---
+++ Samples/Media/Hlms/Unlit/GLSL/PixelShader_ps.glsl
@@ -0,0 +1,53 @@
+@insertpiece( SetCrossPlatformSettings )
+
+layout(std140) uniform;
+#define FRAG_COLOR		0
+layout(location = FRAG_COLOR, index = 0) out vec4 outColour;
+
+// START UNIFORM DECLARATION
+@insertpiece( MaterialDecl )
+@insertpiece( InstanceDecl )
+in block
+{
+@insertpiece( VStoPS_block )
+} inPs;
+// END UNIFORM DECLARATION
+
+@property( num_array_textures )uniform sampler2DArray	textureMapsArray[@value( num_array_textures )];@end
+@property( num_textures )uniform sampler2D	textureMaps[@value( num_textures )];@end
+
+@property( diffuse )@piece( MultiplyDiffuseConst )* material.diffuse@end @end
+
+@property( diffuse_map || alpha_test || diffuse )Material material;@end
+
+void main()
+{
+@property( diffuse_map || alpha_test || diffuse )
+	uint materialId	= instance.materialIdx[inPs.drawId];
+	material = materialArray.m[materialId];
+@end
+
+@property( !diffuse_map && !diffuse_map0 )
+@property( hlms_colour && !diffuse_map )	outColour = inPs.colour @insertpiece( MultiplyDiffuseConst );@end
+@property( !hlms_colour && !diffuse )	outColour = vec4(1, 1, 1, 1);@end
+@property( !hlms_colour && diffuse )	outColour = material.diffuse;@end
+@end
+
+@property( diffuse_map )@property( diffuse_map0 )
+	//Load base image
+        outColour = texture( @insertpiece( SamplerOrigin0 ), @insertpiece( SamplerUV0 ) );@end
+
+@foreach( diffuse_map, n, 1 )@property( diffuse_map@n )
+        vec4 topImage@n = texture( @insertpiece( SamplerOrigin@n ), @insertpiece( SamplerUV@n ) );@end @end
+
+@foreach( diffuse_map, n, 1 )@property( diffuse_map@n )
+	@insertpiece( blend_mode_idx@n )@end @end
+
+	@property( hlms_colour )outColour *= inPs.colour @insertpiece( MultiplyDiffuseConst );@end
+	@property( !hlms_colour && diffuse )outColour *= material.diffuse;@end
+@end
+
+@property( alpha_test )
+	if( material.alpha_test_threshold.x @insertpiece( alpha_test_cmp_func ) outColour.a )
+		discard;@end
+}
